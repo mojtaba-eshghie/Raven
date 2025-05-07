54dd754e4e418d7986de83e60c7fc9aefdae536b
Melissa Mazura <melissa.mazura@gmail.com>
2025-05-08 00:31:31 +0200

    feature #15: advance clustering and plotting

--- ethereum_src.py
+++ ethereum_src.py
@@ -36,3 +36,83 @@ def has_ethereum_src(address, max_retries = 1):
             time.sleep(wait_time)
 
     return None
+
+import requests
+import time
+import json
+
+def get_solidity_source(file_name, source_code):
+    cleaned_input = source_code.replace("'", '"').replace(r'\r\n', '\n')
+    #cleaned_input = json.loads(cleaned_input)
+    hi = cleaned_input.get("language", "")
+    print(hi)
+    source_code_str = source_code.strip('{}')
+    try:
+        source_code = json.loads(source_code_str)  # Now source_code is a dictionary
+    except json.JSONDecodeError as e:
+        print(f"Error parsing SourceCode: {e}")
+        source_code = {}
+
+    source_code = source_code.get("sources", {})
+    if file_name in source_code:
+        return source_code["content"]
+    return None
+
+def get_src(address, file, max_retries=2):
+    params = {
+        "module": "contract",
+        "action": "getsourcecode",
+        "address": address,
+        "apikey": API_KEY
+    }
+
+    retries = 0
+    delay = 0.2
+
+    while retries <= max_retries:
+        response = requests.get(base_url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            with open("fuuuuuck.txt", "w") as file:
+                json.dump(data, file, indent=4)
+
+            if "Max daily rate limit reached" in data["result"]:
+                raise ValueError(data["result"])
+
+            if isinstance(data["result"], list) and len(data["result"]) > 0:
+                contract_info = data["result"][0]
+                print(contract_info)
+                source_code = contract_info.get("SourceCode", '')
+
+                
+                if source_code == '':
+                    return ""
+                else:
+                    matched_code = get_solidity_source(file, source_code)
+                    if matched_code:
+                        return matched_code
+                    else:
+                        print(f"File {file} not found in the contract source code.")
+                        return None
+
+        else:
+            print(f"Request failed with status code {response.status_code}, retrying...")
+            retries += 1
+            wait_time = delay * (2 ** retries)
+            time.sleep(wait_time)
+
+    return None
+
+def main():
+    address = "0x43506849d7c04f9138d1a2050bbf3a0c054402dd"
+    file = "FiatTokenV1.sol"
+
+    source_code = get_src(address, file)
+
+    if source_code:
+        print("Source code found:\n", source_code)
+    else:
+        print("No matching file found.")
+
+if __name__ == "__main__":
+    main()
