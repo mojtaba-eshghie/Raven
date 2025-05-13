38880a47e3de53a13287d3cba6ee1fff12bc2df3
Melissa Mazura <melissa.mazura@gmail.com>
2025-05-13 13:40:17 +0200

    extend clustering and plotting algorithms

--- ethereum_src.py
+++ ethereum_src.py
@@ -73,6 +73,10 @@ def get_src(address, max_retries=2):
         response = requests.get(base_url, params=params)
         if response.status_code == 200:
             data = response.json()
+            
+            with open("test11111111.txt", "w") as file:
+                json.dump(data, file, indent=4)
+                
 
             if "Max daily rate limit reached" in data["result"]:
                 raise ValueError(data["result"])
@@ -90,10 +94,10 @@ def get_src(address, max_retries=2):
     return None
 
 def main():
-    address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    address = "0xc7de47b9ca2fc753d6a2f167d8b3e19c6d18b19a"
     file = "FiatTokenV1.sol"
 
-    source_code = get_src(address)
+    source_code = get_src(address)    
 
     if source_code:
         print("Source code found:\n", source_code)
